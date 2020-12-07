f9a7f7ebb58ccca9bb3c7a364e0449a91a4782c3
204s415s <74140214+204s415s@users.noreply.github.com>
2020-12-07 15:07:43 +0900

    Create studio_data.sql

---
+++ src/main/resources/sample/studio_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO sample_db.studio (studio_id,studio) VALUES
+	 (1,'STUDIO-Ash'),
+	 (2,'FRESH DANCE STUDIO'),
+	 (3,'STUDIO Alleyoop'),
+	 (4,'DANCE SCHOOL LAB3(心斎橋)'),
+	 (5,'STUDIO AX'),
+	 (6,'E-N DANCE SCHOOL STUDIO'),
+	 (7,'STUDIO SUNNY HOOD'),
+	 (8,'DANCE SCHOOL LAB3(阿波座)'),
+	 (9,'CAMURO DANCE STUDIO'),
+	 (10,'DANCE STUDIO OSCAR');
