0676fc95e49d4c196d0b0c4f6ae55cbdf313298b
204s415s <74140214+204s415s@users.noreply.github.com>
2020-12-07 14:30:42 +0900

    Update SearchController.java

--- src/main/java/search/controller/SearchController.java
+++ src/main/java/search/controller/SearchController.java
@@ -5,13 +5,20 @@ import java.util.List;
 
 import search.entity.Dancer;
 import search.entity.Search;
-import search.repository.SearchRepository;
+import search.entity.Studio;
+import search.entity.Week;
+import search.form.SearchForm;
 import search.repository.DancerRepository;
+import search.repository.SearchRepository;
+import search.repository.StudioRepository;
+import search.repository.WeekRepository;
 import search.service.SearchService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,16 +38,24 @@ public class SearchController {
 	public SearchRepository searchRepository; 
 	@Autowired
 	public DancerRepository dancerRepository;
+	@Autowired
+	public StudioRepository studioRepository;
+	@Autowired
+	public WeekRepository weekRepository;
 
 	//Search screen
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String index() {
-		return "index";
+        @RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Model model, @ModelAttribute("form") SearchForm form) {
+            List<Studio> stu = infoService.findAllStudio();
+            List<Week> we = infoService.findAllWeek();
+    	    model.addAttribute("stu", stu);
+    	    model.addAttribute("we", we);
+	    return "index";
 	}
     
 	//search results	
   	@RequestMapping(value = "/index/search", method = RequestMethod.GET)
-	public ModelAndView search(ModelAndView mav
+	public ModelAndView search(ModelAndView mav, @ModelAttribute("form") SearchForm form
 			, @RequestParam("dancer") String dancer, @RequestParam("team") String team
 			, @RequestParam(name="studio", required=false) String studio
 			, @RequestParam(name="week", required=false) String week, @RequestParam(name="start", required=false) BigDecimal start
@@ -57,7 +72,11 @@ public class SearchController {
 		mav.addObject("closeFrom", closeFrom);
 		mav.addObject("closeTo", closeTo);
 		mav.addObject("level", level);
+		List<Studio> stu = infoService.findAllStudio();
+		List<Week> we = infoService.findAllWeek();
 		List<Search> result = searchService.findSearch(dancer, team, studio, week, startFrom, startTo, closeFrom, closeTo, level);
+		mav.addObject("stu", stu);
+		mav.addObject("we", we);
 		mav.addObject("result", result);
 		mav.addObject("resultSize", result.size());
 		return mav;
